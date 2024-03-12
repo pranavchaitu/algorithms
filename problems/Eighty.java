4a70641558235d407a2ebaa1b277b657f78e5a68
unknown <pranavchaitu676@gmail.com>
2024-03-12 22:46:57 +0530

    added lc 80

---
+++ problems/Eighty.java
@@ -0,0 +1,21 @@
+public class Eighty {
+	public static void main(String[] args) {
+		int[] arr = {1,1,2,2,2,3,3};
+		int ans = removeDuplicates(arr);
+		System.out.println(ans);
+	}	
+	static int removeDuplicates(int[] nums) {
+		int l = 2;
+		for(int r=2;r<nums.length;r++){
+				if(nums[r] != nums[l-2]){
+						nums[l] = nums[r];
+						l++;
+				}
+		}
+		return l;
+	}
+}
+
+
+	
+
