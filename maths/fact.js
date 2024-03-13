b99f83b7d7a3a551d9d33611cbc43575ebdcf56a
unknown <pranavchaitu676@gmail.com>
2024-03-13 13:27:48 +0530

    modifiedfact

--- maths/fact.js
+++ maths/fact.js
@@ -6,3 +6,16 @@ function fact(n) {
 }
 
 console.log(fact(4)); //to be 24
+
+
+//non recursive way
+function fact2(n) {
+	let fact = 1;
+	while(n > 0){
+			fact *= n;
+			n--;
+	}
+	return fact;
+}
+
+console.log(fact2(4));
