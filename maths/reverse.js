f5304173b23875d3ce625f2dc9b431e68cbbf51d
unknown <pranavchaitu676@gmail.com>
2024-03-13 13:01:19 +0530

    corrected /maths

--- maths/reverse.js
+++ maths/reverse.js
@@ -1,11 +1,11 @@
 const reverse = (n) => {
-	let num = n;
+	let num = Math.abs(n);
 	let rev = 0;
 	while(num !== 0){
 		rev = rev*10 + num%10; 
 		num = Math.floor(num/10);
 	}
-	return rev > Math.pow(2,31) - 1 ? 0 : num < 0 ? -rev : rev;
+	return rev > Math.pow(2,31) - 1 ? 0 : n < 0 ? -rev : rev;
 }
 
-console.log(reverse(134)); //should be 431
+console.log(reverse(-134)); //should be 431
