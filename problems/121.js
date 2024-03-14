7c03c39f8cf5a7e4144627193b7de34d7356e853
unknown <pranavchaitu676@gmail.com>
2024-03-14 22:21:40 +0530

    solved best-time-to-buy-and-sell-stocks

---
+++ problems/121.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+	let max = 0;
+	let l = 0;
+	let r = 1;
+	while( r < prices.length ){
+		if(prices[l] < prices[r]) {
+			max = Math.max(max,prices[r] - prices[l]);
+		} else {
+			l = r;
+		}
+		r++;
+	}
+	return max;
+};
+
+let ans = maxProfit([7,6,4,2,1,8])
+console.log(ans);
