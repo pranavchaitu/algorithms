6e4a1ec29610a993abe3c4567d3f9cb2e1aca4c3
unknown <pranavchaitu676@gmail.com>
2024-03-15 22:58:35 +0530

    added insertionsort

---
+++ sorting/insertionsort.js
@@ -0,0 +1,14 @@
+function insertionSort(nums) {
+	for(let i = 1;i<nums.length;i++){
+		let j = i - 1;
+		while(j >= 0 && nums[j] > nums[j+1]){
+			let temp = nums[j];
+			nums[j] = nums[j+1];
+			nums[j+1] = temp;
+		}
+	}
+	return nums;
+}
+
+const ans = insertionSort([1,4,2,5,8,55,33]);
+console.log(ans);
