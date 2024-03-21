9f4bc06846a31791a894ff0b0e8956f4afc901d9
unknown <pranavchaitu676@gmail.com>
2024-03-21 16:44:08 +0530

    solved lc 128

---
+++ problems/128-longest-cons-seq.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+  let numSet = new Set(nums);
+	let longest = 0;
+	for(let num of nums){
+		let prev = num - 1;
+		if(numSet.has(prev)){
+			continue;
+		}
+		let [currentNum, score] = [num, 1]
+		const isStreak = () => numSet.has(currentNum + 1);
+		while(isStreak()){
+			currentNum++;
+			score++;
+		}
+		longest = Math.max(longest, score);
+	}
+	return longest;
+};
+
+console.log(longestConsecutive([9,1,4,7,3,-1,0,5,8,-1,6]));
