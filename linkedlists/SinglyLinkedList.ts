cc3d999859697295c667e4fdbe6a0a8959342dc9
pranavchaitu <pranavchaitu676@gmail.com>
2024-04-28 00:39:24 +0530

    added ll pop

--- linkedlists/SinglyLinkedList.ts
+++ linkedlists/SinglyLinkedList.ts
@@ -26,12 +26,48 @@ class SinglyLinkedList<A>{
             this.tail = current
         }
         this.length++
-        console.log(this) 
+        return console.log(this) 
+    }
+    pop() {
+        if(!this.head){
+            return console.log(undefined);
+        }
+        var current = this.head
+        var newTail = this.head
+        while(current?.next){
+            newTail = current
+            current = current.next
+        }
+        this.tail = newTail
+        this.tail.next = null 
+        this.length--
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return console.log(current);
     }
 }
 
 const list = new SinglyLinkedList();
-list.push(2)
+
+console.log(`push here------------------------------------------------------`);
+list.push(true)
 list.push(6)
-list.push(1)
+list.push("chaitu")
 list.push(7)
+list.push("pranav")
+console.log(`push done------------------------------------------------------`);
+
+console.log(`pop here------------------------------------------------------`);
+list.pop()
+list.pop()
+list.pop()
+list.pop()
+list.pop()
+list.pop()
+console.log(`pop done------------------------------------------------------`);
+
+console.log(`------------------------------------------------------`);
+console.log(list);
+console.log(`------------------------------------------------------`);
