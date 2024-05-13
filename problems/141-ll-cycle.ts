4462b2b44927bcb9b61becb1e50b249774ead034
pranavchaitu <pranavchaitu676@gmail.com>
2024-05-13 16:49:26 +0530

    solved ll 141 update

--- problems/141-ll-cycle.ts
+++ problems/141-ll-cycle.ts
@@ -8,14 +8,27 @@ class ListNode {
     }
 }
 
-
+//one step closer (optimal)
 function hasCycle(head: ListNode | null): boolean {
     var slow = head
     var fast = head
     while(fast && fast.next) {
-        slow = slow.next
+        slow = slow!.next
         fast = fast.next.next
         if(slow === fast) return true
     }
     return false
 };
+
+//bruteforce
+// function hasCycle(head: ListNode | null): boolean {
+//     let map = new Map()
+//     while(head){
+//         if(map.has(head)){
+//             return true
+//         }
+//         map.set(head,true) 
+//         head = head.next
+//     }
+//     return false
+// };
