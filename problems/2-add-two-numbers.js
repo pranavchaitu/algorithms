8b4318aff3365b62b82773a1b03fb70e38f4e1a7
pranavchaitu <pranavchaitu676@gmail.com>
2024-05-19 23:53:58 +0530

    init 2

--- problems/2-add-two-numbers.js
+++ problems/2-add-two-numbers.js
@@ -1,25 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 var addTwoNumbers = function(l1, l2) {
-    let dummy = new ListNode();
-    let res = dummy;
-    let total = 0, carry = 0;
-
-    while (l1 || l2 || carry) {
-        total = carry;
-
-        if (l1) {
-            total += l1.val;
-            l1 = l1.next;
-        }
-        if (l2) {
-            total += l2.val;
-            l2 = l2.next;
-        }
-
-        let num = total % 10;
-        carry = Math.floor(total / 10);
-        dummy.next = new ListNode(num);
-        dummy = dummy.next;
+    let num1 = 0
+    let num2 = 0
+    while(l1 && l2){
+        num1 = num1*10 + l1.val
+        num2 = num2*10 + l2.val
+        l1 = l1.next
+        l2 = l2.next
     }
-
-    return res.next;    
+    if(l1) {
+        while(l1){
+            num1 = num1*10 + l1.val
+            l1 = l1.next
+        }
+    } else if(l2){
+        num2 = num2*10 + l2.val
+        l2 = l2.next
+    }
+    console.log(num1+num2)
 };
