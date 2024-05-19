f4abce8ce76d809cc197bb8a378f860a7cef1e78
pranavchaitu <pranavchaitu676@gmail.com>
2024-05-20 00:02:40 +0530

    lc 2atleast tests

--- problems/2-add-two-numbers.js
+++ problems/2-add-two-numbers.js
@@ -24,9 +24,26 @@ var addTwoNumbers = function(l1, l2) {
             num1 = num1*10 + l1.val
             l1 = l1.next
         }
-    } else if(l2){
-        num2 = num2*10 + l2.val
-        l2 = l2.next
+    } else {
+        while(l2){
+            num2 = num2*10 + l2.val
+            l2 = l2.next
+        }
     }
-    console.log(num1+num2)
+    var res = num1 + num2;
+    console.log(res)
+    var rem
+    var dummy = new ListNode()
+    var current = dummy
+    if(res == 0){
+        return new ListNode(0)
+    }
+    while(res) {
+        rem = res % 10
+        current.next = new ListNode(rem)
+        res = Math.floor(res/10)
+        current = current.next   
+    }
+    return dummy.next
 };
+
