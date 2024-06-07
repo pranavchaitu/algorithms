ed7c9a6f389dd3c4d94ca92f768a7aeca94f1e99
pranavchaitu <pranavchaitu676@gmail.com>
2024-06-07 22:36:06 +0530

    solved lc 20

---
+++ problems/20-valid-parentheses.ts
@@ -0,0 +1,18 @@
+function isValid(s: string): boolean {
+    const stack = new Array()
+    for(var i of s){
+        if(i == '(' || i == '[' || i == '{') {
+            stack.push(i)
+        } else {
+            if(!stack.length) return false
+            var top = stack.pop()
+            if((top == '(' && i == ')') || (top == '[' && i == ']') || (top == '{' && i == '}')) continue
+            return false
+        }
+    }
+    return !stack.length
+};
+
+
+console.log(isValid("()[]{}"));
+console.log(isValid("(}"));
