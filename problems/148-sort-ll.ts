a8ff4c51d17fd8125e52d3e6bf4c94e01a754afd
pranavchaitu <pranavchaitu676@gmail.com>
2024-06-14 23:28:55 +0530

    solved lc 148 bf

---
+++ problems/148-sort-ll.ts
@@ -0,0 +1,26 @@
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function sortList(head: ListNode | null): ListNode | null {
+    var res = []
+    var temp1 = head
+    while(head) {
+        res.push(head!.val)
+        head = head.next
+    }
+    var temp2 = temp1
+    head = temp1
+    res = res.sort((a,b) => a-b)
+    for(var i of res) {
+        head!.val = i
+        head = head!.next
+    }
+    return temp2
+};
