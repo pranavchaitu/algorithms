fc6b6bd2a482494cbf090637119cf0c56cbd0a1b
pranavchaitu <pranavchaitu676@gmail.com>
2024-06-16 00:09:24 +0530

    solved lc 83

--- problems/83-remove-duplicates-ll.ts
+++ problems/83-remove-duplicates-ll.ts
@@ -1,25 +1,24 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
     var temp = head
     var next : ListNode | null
-    while(head!.next) {
-        next = head!.next
-        while(head!.val == next!.val) {
-            next = next!.next
+    while(head) {
+        next = head.next
+        while(next && head.val == next.val) {
+            next = next.next
         }
-        head!.next = next
-        head = head!.next
+        head.next = next
+        head = next
     }
     return temp
 };
