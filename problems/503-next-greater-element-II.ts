cb705183796d19819884c04bc9c9d7b705f92ff7
pranavchaitu <pranavchaitu676@gmail.com>
2024-06-29 15:22:35 +0530

    solved lc 503

--- problems/503-next-greater-element-II.ts
+++ problems/503-next-greater-element-II.ts
@@ -1,14 +1,46 @@
+// optimal
 function nextGreaterElements(nums : number[]) : number[] {
-    const stack = [-1]
-    for(var i=nums.length-1;i>=0;i--) {
-        if(stack[stack.length-1] < nums[i]) {
-            while(stack[stack.length-1] < nums[i]) stack.pop()
-        } else {
-            nums[i] = stack[stack.length-1] || -1
+    const n = nums.length;
+    const nge = new Array(n).fill(-1)
+    const stack : number[] = []
+    for(var i=2*n-1;i>=0;i--) {
+        const currentIndex = i % n
+        while(stack && stack[stack.length-1] <= nums[currentIndex]) stack.pop()
+        if(i < n && stack.length) {
+            nge[currentIndex] = stack[stack.length-1]
         }
-        stack.push(nums[i])
+        stack.push(nums[currentIndex])
     }
-    return nums
+    return nge
 }
 
-console.log(nextGreaterElements([1,2,3,4,3]));
+// brute-force
+// function nextGreaterElements(nums: number[]): number[] {
+//     const res = []
+//     var val,j;
+//     for(var i in nums) {
+//         val = -1
+//         j = 0
+//         while(nums[i] !== nums[j]) {
+//             j++
+//         }
+//         while(j < nums.length) {
+//             if(nums[j] > nums[i]) {
+//                 val = nums[j]
+//                 break
+//             }
+//             j++
+//         }
+//         if(val == -1) {
+//             for(var k=0;k<i;k++) {
+//                 if(nums[k] > nums[i]) {
+//                     val = nums[k]
+//                     break
+//                 }
+//             }
+//         }
+//         res.push(val)
+//     }
+//     return res    
+// };
+
