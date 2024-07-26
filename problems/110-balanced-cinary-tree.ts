bdba71a15080240cd671a542121d43c0a053d134
pranavchaitu <pranavchaitu676@gmail.com>
2024-07-26 23:35:55 +0530

    solve lc 110

---
+++ problems/110-balanced-cinary-tree.ts
@@ -0,0 +1,29 @@
+// Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function isBalanced(root: TreeNode | null): boolean {
+    var isBalanced = true
+    function dfs(node) {
+        if(!node) {
+            return 0
+        }
+        const left = dfs(node.left)
+        const right = dfs(node.right)
+        const res = Math.abs(left - right)
+        if(res > 1) {
+            isBalanced = false
+        }
+        return 1 + Math.max(left,right)
+    }
+    dfs(root)
+    return isBalanced
+};
