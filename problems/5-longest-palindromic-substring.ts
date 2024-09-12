7e6c71bee9643607c846cc5a18ca21cb717b32cc
pranavchaitu <pranavchaitu676@gmail.com>
2024-09-12 18:32:50 +0530

    solved lc 5

--- problems/5-longest-palindromic-substring.ts
+++ problems/5-longest-palindromic-substring.ts
@@ -1,22 +1,52 @@
-// bruteforce
-
+// optimal
 function longestPalindrome(s: string): string {
-    var max = "";
+    let res = ""
     for(var i=0;i<s.length;i++) {
-        for(var j=i;j<s.length;j++) {
-            const newStr = s.slice(i,j)
-            if(isPalindrome(newStr) && max.length < newStr.length) {
-                max = newStr
+        // odd condition
+        let l = i
+        let r = i
+        while(l >= 0 && r < s.length && s[l] == s[r]) {
+            const str = s.slice(l,r+1)
+            if(str.length > res.length) {
+                res = str 
             }
+            l -= 1
+            r += 1
+        }
+
+        // even condition
+        l = i
+        r = i + 1
+        while(l >= 0 && r < s.length && s[l] == s[r]) {
+            const str = s.slice(l,r+1)
+            if(str.length > res.length) {
+                res = str
+            }
+            l -= 1
+            r += 1
         }
     }
-    return max
+    return res
 };
 
-function isPalindrome(str : string) {
-    return str === str.split('').reverse().join('')
-}
+// bruteforce
+// function longestPalindrome(s: string): string {
+//     var max = "";
+//     for(var i=0;i<s.length;i++) {
+//         for(var j=i+1;j<s.length;j++) {
+//             const newStr = s.slice(i,j+1)
+//             if(isPalindrome(newStr) && max.length < newStr.length) {
+//                 max = newStr
+//             }
+//         }
+//     }
+//     return max
+// };
+
+// function isPalindrome(str : string) {
+//     return str === str.split('').reverse().join('')
+// }
 
 
-const ans = longestPalindrome("abaabac")
+const ans = longestPalindrome("ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy")
 console.log(ans);
