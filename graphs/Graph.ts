a6f3dfcb5b40a1546222bb1c944de01da33feb8a
pranavchaitu <pranavchaitu676@gmail.com>
2024-09-16 16:44:43 +0530

    solved graph removing-vertices-and-edges

--- graphs/Graph.ts
+++ graphs/Graph.ts
@@ -8,10 +8,26 @@ class Graph<A> {
         if(!this.adjacencyList[name]) this.adjacencyList[name] = []
     }
     
-    addEdges(v1 : any,v2 : any) {
+    addEdge(v1 : any,v2 : any) {
         if(this.adjacencyList[v1]) this.adjacencyList[v1].push(v2)
         if(this.adjacencyList[v2]) this.adjacencyList[v2].push(v1)
     }
+    
+    removeEdge(v1 : any,v2 : any) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter((i) => i != v2)        
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter((i) => i != v1)
+    }
+
+    removeVertex(vertex : any) {
+        if(!this.adjacencyList[vertex]) return
+        
+        this.adjacencyList[vertex].forEach((i) => {
+            this.removeEdge(i,vertex)
+        })
+        
+        delete this.adjacencyList[vertex]
+    }
+
 }
 
 const graph = new Graph()
@@ -22,7 +38,12 @@ graph.addVertex("USA")
 
 console.log(graph);
 
-graph.addEdges("Tokyo","India")
-graph.addEdges("Tokyo","USA")
+graph.addEdge("Tokyo","India")
+graph.addEdge("Tokyo","USA")
+graph.addEdge("India","USA")
 
 console.log(graph);
+
+graph.removeVertex("Tokyo")
+
+console.log("After removing Tokyo from our map :",graph);
