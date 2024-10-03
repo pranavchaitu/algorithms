616cfc7c190cefa9470213110fc634b2bc684a13
pranavchaitu <pranavchaitu676@gmail.com>
2024-10-04 00:11:38 +0530

    solved lc 338

--- problems/338-counting-bits.ts
+++ problems/338-counting-bits.ts
@@ -1,3 +1,22 @@
+// brute-force
 function countBits(n: number): number[] {
-    
+    const res = []
+    for(var i=0;i<=n;i++) {
+        res.push(findOnes(i))
+    }
+    return res
 };
+
+function findOnes(n : number) : number {
+    const bin = n.toString(2)
+    let count = 0
+    for(var i of bin) {
+        if(i == "1") {
+            count += 1
+        }
+    }
+    return count
+}
+
+const ans = countBits(5)
+console.log(ans);
