719505906ec660efaa0a97607f31a373625710d2
pranavchaitu <pranavchaitu676@gmail.com>
2024-10-09 23:43:38 +0530

    190 init

---
+++ problems/190-reverse-bits.ts
@@ -0,0 +1,11 @@
+function reverseBits(n: number): number {
+	// const res = n.toString().split('')
+    // console.log(res)
+    const res = parseInt(n.toString(),2)
+    console.log(res);
+    return 1
+};
+
+reverseBits(10111111111111111111111111111111)
+// console.log(res);
+
