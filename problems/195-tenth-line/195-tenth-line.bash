a5f744ce77e53a726d6603aff16eb53a06a6d013
pranavchaitu <pranavchaitu676@gmail.com>
2024-10-29 22:02:13 +0530

    solved lc 195

---
+++ problems/195-tenth-line/195-tenth-line.bash
@@ -0,0 +1,10 @@
+cnt=0
+while read line && [ $cnt -le 10 ]; do
+    let 'cnt = cnt + 1'
+    if [ $cnt -eq 10 ]; then
+        echo $line
+        exit 0
+    fi
+done < file.txt
+
+echo "Less than/ equal 10 lines in the file."
