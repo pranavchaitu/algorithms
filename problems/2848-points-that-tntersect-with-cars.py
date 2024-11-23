df210327eb3e756a3c7c4ad8b80300c2a18d7659
pranavchaitu <pranavchaitu676@gmail.com>
2024-11-23 15:59:17 +0530

    solved lc 2848

--- problems/2848-points-that-tntersect-with-cars.py
+++ problems/2848-points-that-tntersect-with-cars.py
@@ -1,3 +1,14 @@
-class Solution:
-    def numberOfPoints(self, nums: List[List[int]]) -> int:
-        
+def numberOfPoints(nums: list[list[int]]) -> int:
+    linesweep = [0] * 101
+    for start, end in  nums:
+        linesweep[start-1] += 1
+        linesweep[end] -= 1
+    count = 0
+    for i in range(1,len(linesweep)):
+        linesweep[i] += linesweep[i-1]            
+        if linesweep[i] != 0:
+            count += 1
+    return (count + 1, count) [linesweep[0] == 0]
+
+ans = numberOfPoints([[3,6],[1,5],[4,7]])
+print(ans)
