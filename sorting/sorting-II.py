b9a0075ad76714809a14fa1b169d2707da821c5c
pranavchaitu <pranavchaitu676@gmail.com>
2025-01-30 22:17:03 +0530

    did sorting

--- sorting/sorting-II.py
+++ sorting/sorting-II.py
@@ -22,9 +22,33 @@ def quickSort(arr,low,high):
     quickSort(arr,low,partition-1)
     quickSort(arr,partition+1,high)
 
-arr = [1,4,3,2]
-quickSort(arr,0,len(arr)-1)
+# quickSort(arr,0,len(arr)-1)
+
+arr = [7,2,4,3,1]
+
+def merge(l,h,mid,arr):
+    i = l
+    j = mid + 1
+    res = []
+    while i <= mid and j <= h:
+        if arr[i] <= arr[j]:
+            res.append(arr[i])
+            i += 1
+        else:
+            res.append(arr[j])
+            j += 1
+    res.extend(arr[i:mid+1])
+    res.extend(arr[j:h+1])
+    for i in range(l,h+1):
+        arr[i] = res[i-l]
+
+def merge_sort(arr,low,high):
+    if low >= high:
+        return
+    mid = (low + high) // 2
+    merge_sort(arr,low,mid)
+    merge_sort(arr,mid+1,high)
+    merge(low,high,mid,arr)
+
+merge_sort(arr,0,len(arr)-1)
 print(arr)
-
-
-# def merge_sort
